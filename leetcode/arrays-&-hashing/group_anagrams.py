25bd7f081d6afe7131ef88d0523f8716440127de
Dylan Pina <dillon_idgafos@yahoo.com>
2023-06-14 22:57:38 -0500

    leetcode - arrays & hashing - group anagrams

---
+++ leetcode/arrays-&-hashing/group_anagrams.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        
+        print(ans)
+
+        return ans.values()
