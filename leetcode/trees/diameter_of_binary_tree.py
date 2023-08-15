202e72539b5213306b790a399930893ab12248e8
Dylan Pina <>
2023-08-15 16:45:01 -0500

    leetcode - balanced binary tree

---
+++ leetcode/trees/diameter_of_binary_tree.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+
+        def dfs(root):
+            if not root:
+                return -1
+            left = dfs(root.left)
+            right = dfs(root.right)
+            res[0] = max(res[0], 2 + left + right)
+            print(res[0])
+            print(1 + max(left, right))
+            return 1 + max(left, right)
+        
+        dfs(root)
+        return res[0]
