16fbb6cf1ef4a4893538696dd9ded0f20b46d6e4
dylan <dylansp.dev@gmail.com>
2024-06-05 11:44:20 -0400

    leetcode - time needed to buy tickets

---
+++ leetcode/arrays-&-hashing/time_needed_to_buy_tickets.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        res = 0
+        for i, ticket in enumerate(tickets):
+            if i <= k:
+                res += min(ticket, tickets[k])
+            else:
+                res += min(ticket, tickets[k] - 1)
+        return res
