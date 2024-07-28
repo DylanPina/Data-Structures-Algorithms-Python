38d3d3ef644b72fdc672b66bb939a89684abb349
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-07-28 09:48:55 -0500

    leetcode - number of sub arrays of size k avg

--- leetcode/sliding-window/number_of_sub_arrays_size_k_avg.py
+++ leetcode/sliding-window/number_of_sub_arrays_size_k_avg.py
@@ -1,9 +1,10 @@
 from typing import List
 
+
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
         res = 0
-        curSum = sum(arr[:k-1])
+        curSum = sum(arr[: k - 1])
 
         for L in range(len(arr) - k + 1):
             curSum += arr[L + k - 1]
