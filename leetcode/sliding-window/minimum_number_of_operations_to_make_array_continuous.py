611f7fad0fa15472608ec6066db280321f9a38c6
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-09-21 10:49:48 -0500

    leetcode - minimum number of operations to make array continuous

---
+++ leetcode/sliding-window/minimum_number_of_operations_to_make_array_continuous.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        length = len(nums)
+        nums = sorted(set(nums))
+        res = length
+        r = 0
+
+        for l in range(len(nums)):
+            # nums[l], nums[l] + length - 1
+            while r < len(nums) and nums[r] < nums[l] + length:
+                r += 1
+            window = r - l
+            res = min(res, length - window)
+        return res
