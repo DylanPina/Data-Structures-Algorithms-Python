504a0f96af095015092cddb40324d28ebe738f1f
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-11-19 20:10:11 -0500

    leetcode - reverse linked list

--- leetcode/linked-list/reverse_linked_list.py
+++ leetcode/linked-list/reverse_linked_list.py
@@ -1,18 +1,18 @@
 from typing import Optional
 
+
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, cur = ListNode(None, None), head
-
+        prev, cur = None, head
         while cur:
             next = cur.next
             cur.next = prev
             prev = cur
             cur = next
-        
         return prev
