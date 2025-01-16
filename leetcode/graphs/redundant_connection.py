6b2fecdb682586c58977246ef92efb35c446fbe8
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2025-01-15 22:55:04 -0600

    leetcode - redundant connection

--- leetcode/graphs/redundant_connection.py
+++ leetcode/graphs/redundant_connection.py
@@ -3,35 +3,32 @@ from typing import List
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        unionFind = UnionFind(edges)
-
-        for x, y in edges:
-            if not unionFind.union(x, y):
-                return [x, y]
-        return [-1, -1]
-
-
-class UnionFind:
-    def __init__(self, edges):
-        self.edges = edges
         self.parent = {i: i for i in range(1, len(edges) + 1)}
-        self.rank = {i: 1 for i in range(1, len(edges) + 1)}
+        self.rank = {i: 0 for i in range(1, len(edges) + 1)}
 
-    def find(self, x: int) -> int:
-        # Finds the root of x
-        if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+        def findParent(x: int) -> int:
+            if not self.parent[x] == x:
+                return findParent(self.parent[self.parent[x]])
+            return x
 
-    def union(self, x, y) -> bool:
-        x_root, y_root = self.find(x), self.find(y)
+        def union(x1: int, x2: int) -> bool:
+            x1, x2 = findParent(x1), findParent(x2)
 
-        if x_root != y_root:
-            if self.rank[x_root] < self.rank[y_root]:
-                self.parent[x_root] = y_root
-                self.rank[y_root] += self.rank[x_root]
+            if x1 == x2:
+                return False
+
+            if self.rank[x1] > self.rank[x2]:
+                self.parent[x2] = x1
+            elif self.rank[x1] < self.rank[x2]:
+                self.parent[x1] = x2
             else:
-                self.parent[y_root] = x_root
-                self.rank[x_root] += self.rank[y_root]
+                self.parent[x2] = x1
+                self.rank[x1] -= 1
+
             return True
-        return False
+
+        for x1, x2 in edges:
+            if not union(x1, x2):
+                return [x1, x2]
+
+        return [-1, -1]
