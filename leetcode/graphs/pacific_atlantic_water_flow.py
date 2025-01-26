abfda2a04008daa454255f0c78ee122b041fc6a3
Dylan Pina <dylansp.dev@gmail.com>
2025-01-26 15:03:56 -0600

    leetcode - pacific atlantic water flow

--- leetcode/graphs/pacific_atlantic_water_flow.py
+++ leetcode/graphs/pacific_atlantic_water_flow.py
@@ -2,29 +2,36 @@ from typing import List
 
 
 class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+    def pacificAtlantic(self, h: List[List[int]]) -> List[List[int]]:
+        M, N = len(h), len(h[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        pacific, atlantic = set(), set()
 
-        def dfs(r: int, c: int, visit: set, prevHeight: int) -> None:
-            if ((r, c) in visit
-                or not r in range(ROWS)
-                or not c in range(COLS)
-                    or heights[r][c] < prevHeight):
+        def dfs(r: int, c: int, visit: set) -> None:
+            if (r, c) in visit:
                 return
+
             visit.add((r, c))
-            for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-                dfs(r + dr, c + dc, visit, heights[r][c])
 
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
-        for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if (
+                    min(nr, nc) >= 0
+                    and nr < M
+                    and nc < N
+                    and (nr, nc) not in visit
+                    and h[nr][nc] >= h[r][c]
+                ):
+                    dfs(nr, nc, visit)
 
-        res = []
-        for r, c in pac:
-            if (r, c) in atl:
-                res.append([r, c])
-        return res
+        for r in range(M):
+            dfs(r, 0, pacific)
+        for c in range(N):
+            dfs(0, c, pacific)
+
+        for r in range(M):
+            dfs(r, N - 1, atlantic)
+        for c in range(N):
+            dfs(M - 1, c, atlantic)
+
+        return list(pacific & atlantic)
