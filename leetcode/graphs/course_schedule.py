6b85121fe1f0baf8effaa7dbb1af1c59e7a422b4
Dylan Pina <dylansp.dev@gmail.com>
2025-02-08 14:33:29 -0600

    leetcode - course schedule

--- leetcode/graphs/course_schedule.py
+++ leetcode/graphs/course_schedule.py
@@ -1,29 +1,32 @@
-from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        prereqMap = defaultdict(list)
-        for src, dst in prerequisites:
-            prereqMap[src].append(dst)
+        if len(prerequisites) > numCourses:
+            return False
+
+        prereqMap = {i: [] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            prereqMap[course].append(prereq)
 
         visiting = set()
 
-        def dfs(cur: int) -> bool:
-            if cur in visiting:
+        def dfs(course: int) -> bool:
+            if course in visiting:
                 return False
-
-            if cur not in prereqMap:
+            if not prereqMap[course]:
                 return True
 
-            visiting.add(cur)
-            for preq in prereqMap[cur]:
-                if not dfs(preq):
+            visiting.add(course)
+
+            for prereq in prereqMap[course]:
+                if not dfs(prereq):
                     return False
 
-            visiting.remove(cur)
-            del prereqMap[cur]
+            visiting.remove(course)
+            prereqMap[course] = []
+
             return True
 
         for course in range(numCourses):
