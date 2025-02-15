b1ae4e90c17b03220e38015c5f29de156e6e2158
Dylan Pina <dylansp.dev@gmail.com>
2025-02-15 14:56:35 -0600

    leetcode - course schedule IV

--- leetcode/graphs/course_schedule_IV.py
+++ leetcode/graphs/course_schedule_IV.py
@@ -1,26 +1,33 @@
 from typing import List
-from collections import defaultdict
 
 
 class Solution:
-    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        prereqs = defaultdict(list)
+    def checkIfPrerequisite(
+        self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]
+    ) -> List[bool]:
+        prereqMap = {course: [] for course in range(numCourses)}
+        memo = [[-1 for _ in range(numCourses)] for _ in range(numCourses)]
+
         for prereq, course in prerequisites:
-            prereqs[course].append(prereq)
+            prereqMap[course].append(prereq)
+            memo[course][prereq] = True
 
-        def dfs(course: int):
-            if course not in prereqMap:
-                for pre in prereqs[course]:
-                    prereqMap[course] |= dfs(pre)
-            prereqMap[course].add(course)
-            return prereqMap[course]
+        def dfs(course: int, prereq: int) -> bool:
+            if memo[course][prereq] != -1:
+                return bool(memo[course][prereq])
 
-        prereqMap = defaultdict(set)
-        for crs in range(numCourses):
-            dfs(crs)
-        
+            for prereq in prereqMap[course]:
+                if dfs(prereq, prereq):
+                    memo[course][prereq] = 1
+                    return True
+
+            memo[course][prereq] = 0
+            return False
+
         res = []
-        for u, v in queries:
-            res.append(u in prereqs[v])
-        print(prereqMap)
+        for prereq, course in queries:
+            if course == prereq:
+                res.append(False)
+            else:
+                res.append(dfs(course, prereq))
         return res
