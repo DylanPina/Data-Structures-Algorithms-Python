3ce066792c4af1ca0f3facb227a9a79158da57ce
Dylan Pina <dylansp.dev@gmail.com>
2025-04-18 19:08:07 -0500

    leetcode - palindrome partitioning

--- leetcode/backtracking/palindrome_partitioning.py
+++ leetcode/backtracking/palindrome_partitioning.py
@@ -3,28 +3,27 @@ from typing import List
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-        part = []
+        palindromes, partition = [], []
 
         def dfs(i: int) -> None:
-            if i > len(s) - 1:
-                res.append(part[::])
+            if i == len(s):
+                palindromes.append(partition.copy())
                 return
 
             for j in range(i, len(s)):
-                if self.isPali(s, i, j):
-                    part.append(s[i : j + 1])
+                if self.palindrome(s[i : j + 1]):
+                    partition.append(s[i : j + 1])
                     dfs(j + 1)
-                    part.pop()
-            
+                    partition.pop()
+
         dfs(0)
-        return res
-    
-    def isPali(self, s: str, i: int, j: int) -> bool:
+        return palindromes
+
+    def palindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+
         while i < j:
             if s[i] != s[j]:
                 return False
-            i += 1
-            j -= 1
+            i, j = i + 1, j - 1
         return True
-            
