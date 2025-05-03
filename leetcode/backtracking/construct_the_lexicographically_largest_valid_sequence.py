13dc9c7d1df93774c5078688960c7637dd69e06d
Dylan Pina <dylansp.dev@gmail.com>
2025-05-03 13:10:35 -0500

    leetcode - construct the lexicographically largest valid sequence

--- leetcode/backtracking/construct_the_lexicographically_largest_valid_sequence.py
+++ leetcode/backtracking/construct_the_lexicographically_largest_valid_sequence.py
@@ -11,13 +11,15 @@ class Solution:
                 return True
 
             for num in reversed(range(1, n + 1)):
-                if (num in used) or (i + num >= len(res) or (res[i + num])):
+                if num in used or (
+                    (num != 1) and (i + num >= len(res) or res[i + num] != 0)
+                ):
                     continue
 
-                used.add(num)
                 res[i] = num
-                if num > 1:
+                if num != 1:
                     res[i + num] = num
+                used.add(num)
 
                 j = i + 1
                 while j < len(res) and res[j]:
@@ -26,10 +28,10 @@ class Solution:
                 if backtrack(j):
                     return True
 
-                used.remove(num)
                 res[i] = 0
-                if num > 1:
+                if num != 1:
                     res[i + num] = 0
+                used.remove(num)
 
             return False
 
