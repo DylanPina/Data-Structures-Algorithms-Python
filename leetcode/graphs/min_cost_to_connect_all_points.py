811c01dae12f6d7edd2ccdd139970745e93ec9f1
Dylan Pina <dylansp.dev@gmail.com>
2025-05-09 07:30:47 -0500

    leetcode - min cost to connect all points

--- leetcode/graphs/min_cost_to_connect_all_points.py
+++ leetcode/graphs/min_cost_to_connect_all_points.py
@@ -1,38 +1,39 @@
+import heapq
 from collections import defaultdict
-from heapq import heappop, heappush
 from typing import List
 
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        if len(points) == 1:  # Edge case
-            return 0
+        def manhattan_distance(x_i: int, y_i: int, x_j: int, y_j: int) -> int:
+            return abs(x_i - x_j) + abs(y_i - y_j)
 
-        # Build adjacency list using indices for clarity
         adj = defaultdict(list)
         for i in range(len(points)):
-            xi, yi = points[i]
-            for j in range(i + 1, len(points)):
-                xj, yj = points[j]
-                dist = abs(xi - xj) + abs(yi - yj)
-                adj[i].append((dist, j))
-                adj[j].append((dist, i))
+            x_i, y_i = points[i]
+            for j in range(len(points)):
+                x_j, y_j = points[j]
+                dist = manhattan_distance(x_i, y_i, x_j, y_j)
+                adj[i].append((j, dist))
+                adj[j].append((i, dist))
 
-        # Prim's Algorithm: Min-Heap to store (weight, point)
-        minHeap = [(0, 0)]  # Start with point 0
-        visit = set()
         res = 0
+        min_heap = []
+        for j, d_j in adj[0]:
+            heapq.heappush(min_heap, (d_j, j))
+        visit = set()
 
         while len(visit) < len(points):
-            weight, curr = heappop(minHeap)
-            if curr in visit:
+            d_i, i = heapq.heappop(min_heap)
+
+            if i in visit:
                 continue
 
-            visit.add(curr)
-            res += weight
+            res += d_i
+            visit.add(i)
 
-            for nextWeight, nextPoint in adj[curr]:
-                if nextPoint not in visit:
-                    heappush(minHeap, (nextWeight, nextPoint))
+            for j, d_j in adj[i]:
+                if j not in visit:
+                    heapq.heappush(min_heap, (d_j, j))
 
-        return res
+        return res if len(visit) == len(points) else -1
